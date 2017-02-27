5fd21769167f0bb380e6590702e6a94288067783
Nathan Ross <nross.se@gmail.com>
2017-02-26 18:02:03 -0800

    Create custom.ps1

---
+++ custom.ps1
@@ -0,0 +1,7 @@
+#to automate or do manually on each install
+# disable Windows Search service
+# install chrome, login to correct chrome profile
+# install lastpass
+# install and configure a text editor (sublime or notepad)
+# install vlc
+# install sumatrapdf
