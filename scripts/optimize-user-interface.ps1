0f0b35a8ad29ce5b81b663bc494a204ca8e0230c
Nathan Ross <nross.se@gmail.com>
2017-02-26 17:50:47 -0800

    Update optimize-user-interface.ps1

--- scripts/optimize-user-interface.ps1
+++ scripts/optimize-user-interface.ps1
@@ -9,36 +9,22 @@ Import-Module -DisableNameChecking $PSScriptRoot\..\lib\take-own.psm1
 echo "Elevating priviledges for this process"
 do {} until (Elevate-Privileges SeTakeOwnershipPrivilege)
 
-echo "Apply MarkC's mouse acceleration fix"
-sp "HKCU:\Control Panel\Mouse" "MouseSensitivity" "10"
-sp "HKCU:\Control Panel\Mouse" "MouseSpeed" "0"
-sp "HKCU:\Control Panel\Mouse" "MouseThreshold1" "0"
-sp "HKCU:\Control Panel\Mouse" "MouseThreshold2" "0"
-sp "HKCU:\Control Panel\Mouse" "SmoothMouseXCurve" ([byte[]](0x00, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0xC0, 0xCC, 0x0C, 0x00, 0x00, 0x00, 0x00, 0x00,
-0x80, 0x99, 0x19, 0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x66, 0x26, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x33, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00))
-sp "HKCU:\Control Panel\Mouse" "SmoothMouseYCurve" ([byte[]](0x00, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00,
-0x00, 0x00, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA8, 0x00, 0x00,
-0x00, 0x00, 0x00, 0x00, 0x00, 0xE0, 0x00, 0x00, 0x00, 0x00, 0x00))
+#echo "Disable mouse pointer hiding"
+#sp "HKCU:\Control Panel\Desktop" "UserPreferencesMask" ([byte[]](0x9e,
+#0x1e, 0x06, 0x80, 0x12, 0x00, 0x00, 0x00))
 
-echo "Disable mouse pointer hiding"
-sp "HKCU:\Control Panel\Desktop" "UserPreferencesMask" ([byte[]](0x9e,
-0x1e, 0x06, 0x80, 0x12, 0x00, 0x00, 0x00))
-
-echo "Disable Game DVR and Game Bar"
-force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR"
-sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" "AllowgameDVR" 0
+#echo "Disable Game DVR and Game Bar"
+#force-mkdir "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR"
+#sp "HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR" "AllowgameDVR" 0
 
 echo "Disable easy access keyboard stuff"
 sp "HKCU:\Control Panel\Accessibility\StickyKeys" "Flags" "506"
 sp "HKCU:\Control Panel\Accessibility\Keyboard Response" "Flags" "122"
 sp "HKCU:\Control Panel\Accessibility\ToggleKeys" "Flags" "58"
 
-echo "Restoring old volume slider"
-force-mkdir "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC"
-sp "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC" "EnableMtcUvc" 0
+#echo "Restoring old volume slider"
+#force-mkdir "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC"
+#sp "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC" "EnableMtcUvc" 0
 
 echo "Setting folder view options"
 sp "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" "Hidden" 1
@@ -78,6 +64,6 @@ rm "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpac
 rm "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{A0953C92-50DC-43bf-BE83-3742FED03C9C}"
 rm "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{f86fa3ab-70d2-4fc7-9c99-fcbf05467f3a}"
 
-#echo "Disabling tile push notification"
-#force-mkdir "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"
-#sp "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" "NoTileApplicationNotification" 1
+echo "Disabling tile push notification"
+force-mkdir "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications"
+sp "HKCU:\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\PushNotifications" "NoTileApplicationNotification" 1
